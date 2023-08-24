e98fc7e85f7bb930139325f8790084fdd48009b5
Machoka Daisy <machokadaisy25@gmail.com>
2023-08-24 11:27:30 +0300

    adds daisy component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Daisy from './Daisy';
 
 function App() {
   return (
     <div className="App">
-     
+     <Daisy/>
     </div>
   );
 }
---
+++ src/Daisy.js
@@ -0,0 +1,7 @@
+function Daisy(){
+    return <div>
+        <h1> Hi My name is daisy</h1>
+    </div>
+}
+
+export default Daisy
