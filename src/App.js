bf0f2613375b97f4ec176361b20766c5fb314cc1
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-08-24 11:27:44 +0300

    adds Medrine component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Medrine from './Medrine';
 
 function App() {
   return (
     <div className="App">
-     
+     <Medrine />
     </div>
   );
 }
---
+++ src/Medrine.js
@@ -0,0 +1,6 @@
+function Medrine(){
+    return <div>
+        <h1>Hi my name is Medrine</h1>
+    </div>
+}
+export default Medrine
