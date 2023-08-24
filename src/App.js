eadce35d0bebd15fb6f9b1dd4da3c507e2bcf9e8
ASUS <you@example.com>
2023-08-24 11:31:43 +0300

    add muriithi component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Muriithi from './Muriithi';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <Muriithi/>
     </div>
   );
 }
